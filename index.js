0b2e0219b7f63e5b6f1b8f96ebe7935b892eabeb
Arti-Khillare <agkhillare@gmail.com>
2022-09-05 13:13:11 +0530

    adding updated files

--- index.js
+++ index.js
@@ -1,51 +1,73 @@
-const { response } = require('express');
-const express = require('express');
+const { response } = require("express");
+const express = require("express");
 const app = express();
 const port = 3000;
 const fs = require("fs");
 const puppeteer = require("puppeteer");
+const AdmZip = require("adm-zip");
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send("Hello API")
-})
+/**
+ * Request for First time loading HTML Pages
+ */
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.get('/downloadimage', async (request, response) => {
+
+/**
+ * Main Executing API which takes url, grabs the images zips them and makes available for download
+ */
+app.get("/downloadimage", async (request, response) => {
   try {
-    const queryImage = request.query.image
-    console.log(request)
-    
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const pageURL = "https://www.growpital.com";
-    
+    const pageURL = request.query.url;
 
     let counter = 0;
+    const folderName = `${pageURL.split("/")[2]}`;
     page.on("response", async (response) => {
-        const matches = /.*\.(jpg|png|svg|gif)$/.exec(response.url());
+      const matches = /.*\.(jpg|png|svg|gif)$/.exec(response.url());
 
-        const folderName = `${pageURL.split("/")[2]}`;
-        if (matches && matches.length === 2) {
-            const array = matches[0].split("/");
-            const imageName = array[array.length - 1];
-            if (!fs.existsSync(folderName)) {
-                fs.mkdirSync(folderName);
-            }
-            const buffer = await response.buffer();
-            if (!fs.existsSync(`${folderName}/${imageName}`)) {
-                fs.writeFileSync(`${folderName}/${imageName}`, buffer, "base64");
-            }
-            counter += 1;
+      if (matches && matches.length === 2) {
+        const array = matches[0].split("/");
+        const imageName = array[array.length - 1];
+        if (!fs.existsSync(folderName)) {
+          fs.mkdirSync(folderName);
         }
+        const buffer = await response.buffer();
+        if (!fs.existsSync(`${folderName}/${imageName}`)) {
+          fs.writeFileSync(`${folderName}/${imageName}`, buffer, "base64");
+        }
+        counter += 1;
+      }
     });
 
     await page.goto(pageURL, { timeout: 0 });
+    createzip(`${folderName}.zip`, folderName).then((data) => {
+      console.log("see data:-", data);
+    });
+
     await browser.close();
-    response.send()
-    }
-    catch (err) {
-        res.send({ msg: 'invalid command', error: err })
-    }
+    response.download(`${folderName}.zip`);
+  } catch (err) {
+    response.send({ msg: "invalid command", error: err });
+  }
 });
+
+/**
+ * Function to zip Folder
+ * @param {*} fileName 
+ * @param {*} folderPath 
+ * @returns 
+ */
+function createzip(fileName, folderPath) {
+  return new Promise((resolve, reject) => {
+    const zip = new AdmZip();
+    zip.addLocalFolder(folderPath);
+    zip.writeZip(fileName);
+    return resolve("done");
+  });
+}
 
 app.listen(port, () => console.log(`server is listening on port ${port}!`))
