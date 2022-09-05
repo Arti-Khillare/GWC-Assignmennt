058366923218de484eed422f69c8d68399ebd083
Arti-Khillare <agkhillare@gmail.com>
2022-09-05 11:15:45 +0530

    adding html file

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-const fs = require('fs');
-const puppeteer = require('puppeteer');
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+app.use(express.json());
 
+app.get('/', (req,res) => {
+    res.send("Hello API")
+})
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+app.get('/downloadimage', async (request, response) => {
+  try {
+    const queryImage = request.query.image
+    console.log(request)
+    
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const pageURL = "https://www.growpital.com";
+    
 
-  let counter = 0;
-  page.on('response', async (response) => {
-    const matches = /.*\.(jpg|png|svg|gif)$/.exec(response.url());
-    console.log(matches);
-    if (matches && (matches.length === 2)) {
-      const extension = matches[1];
-      const buffer = await response.buffer();
-      fs.writeFileSync(`images/image-${counter}.${extension}`, buffer, 'base64');
-      counter += 1;
+    let counter = 0;
+    page.on("response", async (response) => {
+        const matches = /.*\.(jpg|png|svg|gif)$/.exec(response.url());
+
+        const folderName = `${pageURL.split("/")[2]}`;
+        if (matches && matches.length === 2) {
+            const array = matches[0].split("/");
+            const imageName = array[array.length - 1];
+            if (!fs.existsSync(folderName)) {
+                fs.mkdirSync(folderName);
+            }
+            const buffer = await response.buffer();
+            if (!fs.existsSync(`${folderName}/${imageName}`)) {
+                fs.writeFileSync(`${folderName}/${imageName}`, buffer, "base64");
+            }
+            counter += 1;
+        }
+    });
+
+    await page.goto(pageURL, { timeout: 0 });
+    await browser.close();
+    response.send()
     }
-  });
+    catch (err) {
+        res.send({ msg: 'invalid command', error: err })
+    }
+});
 
-  //await page.goto('https://www.bannerbear.com/solutions/automate-your-marketing/');
-  await page.goto('https://www.growpital.com', {timeout: 0});
-    //await page.goto('https://unsplash.com/s/photos/the-source')
-
-  await browser.close();
-})();
+app.listen(port, () => console.log(`server is listening on port ${port}!`))
